15b4fec70be78f9240cefde3757acdf15c82557f
mgow01 <m.gow01@gmail.com>
2024-06-10 19:01:00 +0200

    added input validation added support for lower case DNA bases

--- app.R
+++ app.R
@@ -15,6 +15,8 @@ ui <- fluidPage(
                 rows = 6),
   
   actionButton("submit", "Submit"),
+  conditionalPanel(condition = "input.submit != 0",
+                   textOutput("error_msg")),
   tableOutput("table"),
   uiOutput("downloadData"),
 )
@@ -23,59 +25,81 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   rv <- reactiveValues(
     df = data.frame("gR Number" = as.numeric(),
-                     "gRNA Sequence" = as.character(),
-                     "Oligo Sequence to Order" = as.character(),
-                     "Oligo Name" = as.character()
-                     )
+                    "gRNA Sequence" = as.character(),
+                    "Oligo Sequence to Order" = as.character(),
+                    "Oligo Name" = as.character()
+    ),
+    seq_in = NULL
   )
   
   observeEvent(input$submit, {
-    fw5 <- DNAString("AAGTT")
-    fw3 <- DNAString("G")
-    rv5 <- DNAString("AAAAC")
-    rv3 <- DNAString("A")
-    gR <- unlist(str_extract_all(input$entry, "gR\\d+"))
-    gRNA <- unlist(str_extract_all(input$entry, "(?<=[[:blank:]])\\w+"))
-    gRNA <- DNAStringSet(gRNA)
-    gRNA_rc <- reverseComplement(gRNA)
-    gRNA <- c(rbind(gRNA, gRNA_rc))
-    gRNA <- DNAStringSet(gRNA)
+    rv$seq_in <- unlist(str_extract_all(input$entry, "(?<=[[:blank:]])\\w+")) %>% 
+      str_split(pattern = "") %>% unlist() %>% toupper()
     
-    oligo <- gRNA
-    for (i in seq_along(oligo)) {
-      if (i %% 2 == 0) {
-        oligo[[i]] <- c(rv5, oligo[[i]], rv3)
+    if (isTRUE(rv$seq_in %in% DNA_BASES %>% all())) {
+      fw5 <- DNAString("AAGTT")
+      fw3 <- DNAString("G")
+      rv5 <- DNAString("AAAAC")
+      rv3 <- DNAString("A")
+      gR <- unlist(str_extract_all(input$entry, "gR\\d+"))
+      gRNA <- unlist(str_extract_all(input$entry, "(?<=[[:blank:]])\\w+")) %>% 
+        toupper()
+      
+      gRNA <- DNAStringSet(gRNA)
+      gRNA_rc <- reverseComplement(gRNA)
+      gRNA <- c(rbind(gRNA, gRNA_rc))
+      gRNA <- DNAStringSet(gRNA)
+      
+      oligo <- gRNA
+      for (i in seq_along(oligo)) {
+        if (i %% 2 == 0) {
+          oligo[[i]] <- c(rv5, oligo[[i]], rv3)
+        }
+        else {
+          oligo[[i]] <- c(fw5, oligo[[i]], fw3)
+        }
       }
-      else {
-        oligo[[i]] <- c(fw5, oligo[[i]], fw3)
-      }
+      
+      # labelling oligo names to indicate forward and reverse
+      oligo_name <- rep(gR, each = 2)
+      for (i in 1:length(oligo_name)) {
+        if (i %% 2 == 0) {
+          oligo_name[i] <- paste(oligo_name[i], "Rv")
+        }
+        else {
+          oligo_name[i] <- paste(oligo_name[i], "Fw")
+        }}
+      
+      #duplicating each gR number and then adding "revcom to the 2nd"
+      gR <- rep(gR, each = 2)
+      for (i in 1:length(gR)) {
+        if (i %% 2 == 0) {
+          gR[i] <- paste(gR[i], "reverse complemented")
+        }
+        else {
+          gR[i] <- paste(gR[i])
+        }}
+      
+      rv$df <- data.frame("gR Number" = gR,
+                          "gRNA Sequence" = gRNA,
+                          "Oligo Sequence to Order" = oligo,
+                          "Oligo Name" = oligo_name,
+                          check.names = FALSE)
     }
-    
-    # labelling oligo names to indicate forward and reverse
-    oligo_name <- rep(gR, each = 2)
-    for (i in 1:length(oligo_name)) {
-      if (i %% 2 == 0) {
-        oligo_name[i] <- paste(oligo_name[i], "Rv")
-      }
-      else {
-        oligo_name[i] <- paste(oligo_name[i], "Fw")
-      }}
-    
-    #duplicating each gR number and then adding "revcom to the 2nd"
-    gR <- rep(gR, each = 2)
-    for (i in 1:length(gR)) {
-      if (i %% 2 == 0) {
-        gR[i] <- paste(gR[i], "reverse complemented")
-      }
-      else {
-        gR[i] <- paste(gR[i])
-      }}
-    
-    rv$df <- data.frame("gR Number" = gR,
-                        "gRNA Sequence" = gRNA,
-                        "Oligo Sequence to Order" = oligo,
-                        "Oligo Name" = oligo_name,
-                        check.names = FALSE)
+  })
+  
+  # output$error_msg <- (renderText({
+  #   validate(need(isFALSE(rv$seq_in %in% DNA_BASES %>% all()),
+  #                 message = "Error: input contains invalid DNA base"))
+  # })
+  # )
+  
+  output$error_msg <- renderText({
+    if (isFALSE(rv$seq_in %in% DNA_BASES %>% all())) {
+      print("Error: input contains invalid DNA base")
+    } else {
+      return()
+    }
   })
   
   output$table <- renderTable({
